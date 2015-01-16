941f8104ebc5eaa5024dc8fb3c89c486c9741773
puneetkhanal <puneetkhanal@gmail.com>
2015-01-15 21:36:52 -0600

    handled file not found exception

--- SearchExercise/src/search/SearchExercise.groovy
+++ SearchExercise/src/search/SearchExercise.groovy
@@ -14,46 +14,50 @@ import org.mapdb.*;
 class SearchExercise {
 	
     def execute(def args){
-        if(args.size()<2){
-            log.error 'insufficient arguments'
-            System.exit(0)
-        }
-        def fileReader=new FileReader()
-        def indexService=new Indexer()
-        def basePath=args[0]
-        def searchString=args[1]
-        def replaceString
-        if(args.size()>=3){
-            replaceString=args[2]
-        }
-        def outputfile
-        if(args.size()==4){
-            outputfile=args[3]
-        }
+        try{
+            if(args.size()<2){
+                log.error 'insufficient arguments'
+                System.exit(0)
+            }
+            def fileReader=new FileReader()
+            def indexService=new Indexer()
+            def basePath=args[0]
+            def searchString=args[1]
+            def replaceString
+            if(args.size()>=3){
+                replaceString=args[2]
+            }
+            def outputfile
+            if(args.size()==4){
+                outputfile=args[3]
+            }
         
-        // inject map db for persistence map
-        indexService.setMap(DBMaker.newTempTreeMap())
-        searchString=QueryProcessor.parseQuery(searchString);
+            // inject map db for persistence map
+            indexService.setMap(DBMaker.newTempTreeMap())
+            searchString=QueryProcessor.parseQuery(searchString);
         
-        def currentTime=System.currentTimeMillis()
+            def currentTime=System.currentTimeMillis()
         
-        fileReader.setIndexService(indexService)
-        fileReader.readFromDirectory(basePath);
+            fileReader.setIndexService(indexService)
+            fileReader.readFromDirectory(basePath);
         
-        log.info 'Total indexing time: '+(System.currentTimeMillis()-currentTime)+' ms'
+            log.info 'Total indexing time: '+(System.currentTimeMillis()-currentTime)+' ms'
         
-        currentTime=System.currentTimeMillis()
-        def results=indexService.search(searchString)
+            currentTime=System.currentTimeMillis()
+            def results=indexService.search(searchString)
        
-        log.info 'Total search time: '+(System.currentTimeMillis()-currentTime)+' ms'
+            log.info 'Total search time: '+(System.currentTimeMillis()-currentTime)+' ms'
         
-        if(replaceString==null){
-            fileReader.processResults(results,searchString)
-        }else if(outputfile==null){
-            fileReader.processResults(results,searchString,replaceString)
-        }else{
-            log.info outputfile
-            fileReader.processResults(results,searchString,replaceString,outputfile)
+            if(replaceString==null){
+                fileReader.processResults(results,searchString)
+            }else if(outputfile==null){
+                fileReader.processResults(results,searchString,replaceString)
+            }else{
+                log.info outputfile
+                fileReader.processResults(results,searchString,replaceString,outputfile)
+            }
+        }catch(FileNotFoundException e){
+            log.error e.getMessage()+" Not found"
         }
     }
     
